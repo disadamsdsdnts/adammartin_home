a23cc97c357240288444f42627263771b00827cc
Adan Martin <adanmartin@baetica.com>
2023-05-14 09:18:31 +0200

    DEV: Inicial y final

---
+++ js/miscodigos.js
@@ -0,0 +1,21 @@
+		$(document).ready(function() {
+			$('#carouselFotografia').carousel({
+	                interval: 5000
+	        });
+	 
+	        //Handles the carousel thumbnails
+	        $('[id^=carousel-selector-]').click(function () {
+	        var id_selector = $(this).attr("id");
+	        try {
+	            var id = /-(\d+)$/.exec(id_selector)[1];
+	            console.log(id_selector, id);
+	            jQuery('#carouselFotografia').carousel(parseInt(id));
+	        } catch (e) {
+	            console.log('Regex failed!', e);
+	        }
+	    });
+	        $('#carouselFotografia').on('slid.bs.carousel', function (e) {
+	                 var id = $('.item.active').data('slide-number');
+	                $('#carousel-text').html($('#slide-content-'+id).html());
+	        });
+		});
